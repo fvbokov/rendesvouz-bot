9fd82e0b6a2befd35eb4a6c7298ad7a3278cbfa4
rendezv0us <>
2021-08-24 16:28:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from webserver import keep_alive
+import os
+
+import discord
+import re
+from bs4 import BeautifulSoup
+import requests
+from random import randint
+
+youtube_links = []
+
+with open('result.txt', 'r') as f:
+	string = f.read()
+youtube_links = string.split('\n')
+
+class MyClient(discord.Client):
+	async def on_ready(self):
+		print('Logged on as {0}!'.format(self.user))
+
+	async def on_message(self, message):
+		channel = message.channel
+		if message.content.startswith('/who'):
+			with open(str(randint(1, 12)) + '.png', 'rb') as f:
+				picture = discord.File(f)
+				await channel.send(file=picture)
+		if message.content.startswith('/roll'):
+			if message.content == '/roll':
+				await channel.send('{0.author} rolls {1}'.format(message, randint(0, 100)))
+			else:
+				regex = re.compile('^/roll ([0-9]+)-([0-9]+)$')
+				match = regex.match(message.content)
+				
+				if match is None:
+					await channel.send('Wrong input. Example of correct input: "/roll 89-777"')
+					return
+				else:
+					await channel.send('{0.author} rolls {1}'.format(message, randint(int(match.group(1)), int(match.group(2)))))
+		if message.content == '/flip':
+			n = randint(0, 1)
+			if n == 0:
+				await channel.send('{0.author} flips HEADS'.format(message))
+			else:
+				await channel.send('{0.author} flips TAILS'.format(message))
+		if message.content == '/anekdot':
+				s=requests.get('http://anekdotme.ru/random')
+				b=BeautifulSoup(s.text, "html.parser")
+				p=b.select('.anekdot_text')
+				for x in p:
+					s=(x.getText().strip())
+					await channel.send(s)
+					break
+    if message.content == '/yt':
+      url = youtube_links[randint(0, len(youtube_links)-1]
+      await channel.send(url)
+
+		
+client = MyClient()
+
+#Bottom of Main.py
+keep_alive()
+
+TOKEN = os.environ['DISCORD_BOT_SECRET']
+
+client.run(TOKEN)
+
